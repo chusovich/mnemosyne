0dd0f568fcc3d961831320ad3a82c3a81098f8f4
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-02-19 08:40:20 -0500

    Add NixOS configuration for mnemosyne system

--- flake.nix
+++ flake.nix
@@ -1,1 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nix-core = {
+      url = "github:chusovich/nix-core/0.1.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
+  outputs = { self, nixpkgs, nix-core, ... }: {
+    nixosConfigurations.mnemosyne = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./configuration.nix
+        nix-core.serverModules.default
+      ];
+    };
+  };
+}

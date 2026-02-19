9ac7d80ed3d9bba393c0c3e3ef3e1204e29c95a2
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-02-19 09:41:23 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,41 @@
   services.zfs.autoScrub.enable = true;
   networking.hostId = "4e24220d"; # so ZFS can identify the server
 
+  # Restic Backups
+  services.restic.backups = {
+    immich = {
+      user = "mnemosyne";
+      repository = "sftp:backup@192.168.1.100:/backups/‹name›"
+      initialize = true; # initializes the repo, don't set if you want manual control
+      passwordFile = "<path>";
+      paths = [ "/mnt/immich" ];
+      timerConfig = {
+        onCalendar = "saturday 03:00";
+      };
+      pruneOpts = [
+        "--keep-weekly 5"
+        "--keep-monthly 24"
+        "--keep-yearly 75"
+      ]
+    };
+    paperless = {
+      user = "mnemosyne";
+      repository = "sftp:backup@192.168.1.100:/backups/‹name›"
+      initialize = true; # initializes the repo, don't set if you want manual control
+      passwordFile = "<path>";
+      paths = [ "/mnt/paperless" ];
+      timerConfig = {
+        onCalendar = "saturday 03:15";
+      };
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 5"
+        "--keep-monthly 12"
+        "--keep-yearly 75"
+      ]
+    };
+  };
+
   # Network Configuration
   networking = {
     hostName = "mnemosyne";
